f4193aa664edfc6f702b545dc85c5dd61fea3ae4
Penial <62630906+PikachuPenial@users.noreply.github.com>
2022-02-15 13:08:55 -0800

    Executions :)
    gg wp again

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -12,7 +12,7 @@ function GM:PlayerSpawn(ply)
     ply:SetWalkSpeed(175)
     ply:SetJumpPower(230)
 
-    local playerModels = {"models/kyo/ghost_azrael_pm.mdl", "models/balkandude.mdl", "models/usef.mdl", "models/tfusion/playermodels/mw3/mp_delta_elite_a.mdl", "models/kuma96/codmw/characters/allegiancedef/allegiancedef_pm.mdl"}
+    local playerModels = {"models/kyo/ghost_azrael_pm.mdl", "models/balkandude.mdl", "models/usef.mdl", "models/tfusion/playermodels/mw3/mp_delta_elite_a.mdl", "models/kuma96/codmw/characters/allegiancedef/allegiancedef_pm.mdl", "models/tfusion/playermodels/mw3/mp_sas_c.mdl"}
     local chosenPlayerModel = playerModels[math.random(#playerModels)]
 
     ply:SetModel(playerModels[math.random(#playerModels)])
--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -77,7 +77,58 @@ RunConsoleCommand("mgbase_saveatts", "0")
 RunConsoleCommand("mgbase_sv_accuracy", "1")
 RunConsoleCommand("mgbase_sv_pvedamage", "1")
 RunConsoleCommand("mgbase_sv_pvpdamage", "1")
-RunConsoleCommand("mgbase_sv_recoil", "2")
+RunConsoleCommand("mgbase_sv_recoil", "1.7")
 
   --Modern Warfare Base Setup / Allow Attachments
 RunConsoleCommand("mgbase_sv_customization", "1")
+
+  --HUD Setup
+RunConsoleCommand("gwz_hud_deathcamera_enable", "1")
+RunConsoleCommand("gwz_hud_deathcamera_mode", "1")
+RunConsoleCommand("gwz_hud_enable", "1")
+RunConsoleCommand("gwz_hud_enable_ammo", "1")
+RunConsoleCommand("gwz_hud_enable_armorbreak", "1")
+RunConsoleCommand("gwz_hud_enable_attackindicator", "1")
+RunConsoleCommand("gwz_hud_enable_counter", "1")
+RunConsoleCommand("gwz_hud_enable_debug", "0")
+RunConsoleCommand("gwz_hud_enable_healthpanel", "1")
+RunConsoleCommand("gwz_hud_enable_healthteampanel", "0")
+RunConsoleCommand("gwz_hud_enable_hitmarker", "1")
+RunConsoleCommand("gwz_hud_enable_neardeath", "1")
+RunConsoleCommand("gwz_hud_hitmark_snd_style", "0")
+
+  --Hud Offset/Scale
+RunConsoleCommand("gwz_hud_adaptscale", "0")
+RunConsoleCommand("gwz_hud_offset_h", "0")
+RunConsoleCommand("gwz_hud_offset_v", "0")
+RunConsoleCommand("gwz_hud_scale_multiplier", "1")
+
+  --Hud Preformance
+RunConsoleCommand("gwz_hud_reduce_effect", "0")
+RunConsoleCommand("gwz_hud_enable_shake_on_explosion", "1")
+
+  --Hud Server Setup
+RunConsoleCommand("gwz_hud_server_realism_mode", "0")
+RunConsoleCommand("gwz_hud_server_teambased", "0")
+
+  --Executions Setup
+RunConsoleCommand("bsmod_codmw_anims_chance", "3")
+RunConsoleCommand("bsmod_codv_anims_chance", "3")
+
+  --Executions Debug
+RunConsoleCommand("bsmod_debug_calcview", "0")
+RunConsoleCommand("bsmod_kick_enabled", "0")
+
+  --Executions Modifiers
+RunConsoleCommand("bsmod_killmove_anytime_behind", "1")
+RunConsoleCommand("bsmod_killmove_disable_defaults", "1")
+RunConsoleCommand("bsmod_killmove_enable_npcs", "1")
+RunConsoleCommand("bsmod_killmove_enable_players", "1")
+RunConsoleCommand("bsmod_killmove_enable_teammates", "0")
+RunConsoleCommand("bsmod_killmove_glow", "0")
+RunConsoleCommand("bsmod_killmove_minhealth", "1")
+RunConsoleCommand("bsmod_killmove_spawn_healthkit", "1")
+RunConsoleCommand("bsmod_killmove_spawn_healthvial", "1")
+RunConsoleCommand("bsmod_killmove_stun_npcs", "0")
+RunConsoleCommand("bsmod_killmove_thirdperson", "0")
+RunConsoleCommand("bsmod_killmove_time", "1")
