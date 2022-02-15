bbb2373add286d1bddc9f980a46de772ff0b28f4
Penial <62630906+PikachuPenial@users.noreply.github.com>
2022-02-14 23:15:08 -0800

    Initial creation/setup for gamemode.
    GG!

---
+++ gamemode/shared.lua
@@ -0,0 +1,15 @@
+GM.Name = "Gunfight"
+GM.Author = "Pikachu/Penial"
+GM.Email = "jacksonwassermann55@icloud.com"
+GM.Website = "https://github.com/PikachuPenial"
+
+DeriveGamemode("sandbox")
+
+function GM:initialize()
+  --Do my stuff here ig
+end
+
+function GM:ContextMenuOpen()
+  return false
+end
+
