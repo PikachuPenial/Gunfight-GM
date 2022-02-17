a4ca24d4a1fff6000c52d229d2baf90ed50bda60
PortableTerraria <63077153+PortableTerraria@users.noreply.github.com>
2022-02-16 21:12:38 -0600

    Added match system
    may not entirely work

--- gamemode/lobbyhud.lua
+++ gamemode/lobbyhud.lua
@@ -1,23 +1,76 @@
+local timeLeft = 0
+local timerTypeInt = 0
+
+net.Receive("TimerLength", function(len, ply)
+
+    timeLeft = net.ReadUInt(10)
+
+end)
+
+-- Tracks what kind of timer is active. As of writing; 0 will hide the timer, 1 indicates buy time, and 2 indicated round time.
+net.Receive("ActiveTimerType", function(len, ply)
+
+    timerTypeInt = net.ReadUInt(3)
+
+end)
+
 function HUD()
+
     local client = LocalPlayer()
   
     if !client:Alive() then
         return
-      end
+    end
 
-  --This HUD will appear when a player is in the lobby, and is not in a match.
+    -- Remember kids, if you've been paying attention to my comments like good boys, you would know what this next part does.
 
-      draw.SimpleText("You are currently NOT in a queue!", "DermaLarge", ScrW() / 2, ScrH() - 900, Color(255,0,0,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-      draw.SimpleText("Gunfight BETA", "CloseCaption_Bold", ScrW() - 95, ScrH() - 1055, Color(55,255,55,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+    if timerTypeInt > 0 then
 
-  end
-  hook.Add("HUDPaint", "LobbyHud", HUD)
+        if timerTypeInt == 1 then
+
+            -- Buy Time Text
+
+            draw.RoundedBox(0, (ScrW() / 2) - 70, 20, 140, 40, Color(60,60,60,175))
+            draw.SimpleText("BUY TIME", "DermaLarge", ScrW() / 2, 40, Color(0,0,0,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+
+            -- Buy Time Left
+
+            draw.RoundedBox(0, (ScrW() / 2) - 25, 70, 50, 40, Color(60,60,60,175))
+            draw.SimpleText(timeLeft, "DermaLarge", ScrW() / 2, 90, Color(0,0,0,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+
+        end
+
+        if timerTypeInt == 2 then
+
+            -- Buy Time Text
+
+            draw.RoundedBox(0, (ScrW() / 2) - 90, 20, 180, 40, Color(60,60,60,175))
+            draw.SimpleText("ROUND TIME", "DermaLarge", ScrW() / 2, 40, Color(0,0,0,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+
+            -- Buy Time Left
+
+            draw.RoundedBox(0, (ScrW() / 2) - 50, 70, 100, 40, Color(60,60,60,175))
+            draw.SimpleText(timeLeft, "DermaLarge", ScrW() / 2, 90, Color(0,0,0,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+
+        end
+
+    else
+
+        --This HUD will appear when a player is in the lobby, and is not in a match.    
+        
+        draw.SimpleText("You are currently NOT in a queue!", "DermaLarge", ScrW() / 2, ScrH() - 900, Color(255,0,0,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+        draw.SimpleText("Gunfight BETA", "CloseCaption_Bold", ScrW() - 95, ScrH() - 1055, Color(55,255,55,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+
+    end
+
+end
+hook.Add("HUDPaint", "LobbyHud", HUD)
   
-  function HideHud(name)
+function HideHud(name)
     for k, v in pairs({"CHudHealth", "CHudBattery", "CHudAmmo", "CHudSecondaryAmmo"}) do
         if name == v then
             return false
         end
     end
-  end
-  hook.Add("HUDShouldDraw", "HideDefaultHud", HideHud)
+end
+hook.Add("HUDShouldDraw", "HideDefaultHud", HideHud)
--- gamemode/sv_match_system.lua
+++ gamemode/sv_match_system.lua
@@ -9,15 +9,79 @@ local playerTwo
 local spawnOne
 local spawnTwo
 
-function EndBuyTime(plyTable)
+local knownTimerType
 
-    print("Ending Buy Time!")
+util.AddNetworkString( "TimerLength" )
+util.AddNetworkString( "ActiveTimerType" )
+
+function UpdateTimer(timer, type)
+
+    net.Start("TimerLength")
+    net.WriteUInt(timer, 10)
+    net.Broadcast()
+
+    if(type != knownTimerType) then
+
+        net.Start("ActiveTimerType")
+        net.WriteUInt(type, 3)
+        net.Broadcast()
+
+    end
+
+end
+
+function NextRound(winner, loser)
+
+    local plyTable = {winner, loser}
+    table.Shuffle(plyTable)
+
+    timer.Create("NextRoundTimer", 5, 1, function ()
+    
+        StartBuyTime(plyTable)
+
+    end)
+
+end
+
+hook.Add("PlayerDeath", "Next_Round", function(victim, _, attacker)
+
+    if attacker == victim then return end
+
+    NextRound(attacker, victim)
+
+end)
+
+function StartRound(plyTable)
+
+    print("StartingRound!")
 
     for _, ply in ipairs( plyTable ) do
 
         ply:UnLock()
 
     end
+
+    local roundTime = 90
+
+    local timeLeft = roundTime
+
+    UpdateTimer(timeLeft, 2)
+
+    timer.Create("RoundTimer", 1, 0, function()
+    
+        if timeLeft < 1 then
+
+            timer.Remove("RoundTimer")
+
+        else
+
+            timeLeft = timeLeft - 1
+
+            UpdateTimer(timeLeft, 2)
+
+        end
+    
+    end)
 
 end
 
@@ -31,7 +95,33 @@ function StartBuyTime(plyTable)
 
     end
 
-    timer.Simple( 20, function() EndBuyTime(plyTable) end )
+    local buyTime = 20
+
+    local timeLeft = buyTime
+
+    UpdateTimer(timeLeft, 1)
+
+    timer.Create("BuyTimer", 1, 0, function()
+    
+        if timeLeft < 1 then
+
+            print("starting round")
+
+            StartRound(plyTable)
+
+            timer.Remove("BuyTimer")
+
+        else
+
+            print("TimeLeft = " .. tostring(timeLeft))
+
+            UpdateTimer(timeLeft, 1)
+
+            timeLeft = timeLeft - 1
+
+        end
+    
+    end)
 
 end
 
@@ -48,6 +138,8 @@ function SpawnPlayers(map)
 
         playerOne:SetPos(spawnOne:GetPos())
         playerOne:SetEyeAngles(spawnOne:GetAngles())
+
+        playerOne:PrintMessage( HUD_PRINTTALK, "Only one player is in this game, match will proceed in debug mode for testing purposes. Shit probably won't work correctly, so get a friend." )
 
     else
 
@@ -71,6 +163,11 @@ end
 
 function StartMatch(ply, cmd, args)
 
+    -- Ugly way of disabling all timers if you make a new match mid-game. For testing (:
+
+    local timerNames = {"BuyTimer", "RoundTimer"}
+    for k, v in pairs(timerNames) do if timer.Exists(v) == true then timer.Remove(v) end end
+
     roundMap = args[1]
 
     SpawnPlayers(roundMap)
@@ -80,7 +177,21 @@ concommand.Add("gf_start_match", StartMatch)
 
 function EndMatch()
 
+    -- Ugly way of disabling all timers if you make a new match mid-game. For testing (:
+    local timerNames = {"BuyTimer", "RoundTimer"}
+    for k, v in pairs(timerNames) do if timer.Exists(v) == true then timer.Remove(v) end end
+
+    spawns = ents.FindByClass( "info_player_start" )
+
+    for k, ply in ipairs( player.GetHumans() ) do
+
+        ply:SetPos(spawns[k]:GetPos())
+        ply:UnLock()
+
+    end
+
 end
+concommand.Add("gf_end_match", EndMatch)
 
 function NewRound()
 
